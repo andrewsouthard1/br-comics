a9ac2d827fd489042e8041feb6dbd8337a57de63
Andrew <andrewsouthard111@gmail.com>
2017-05-14 20:48:56 -0400

    Add db info

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,17 +1,17 @@
 class ComicsController < ApplicationController
   def index
     if params[:sort] == "price_low_to_high"
-      @comics = Product.all.order(:price)
+      @comics = Comic.all.order(:price)
     elsif params[:sort] == "price_high_to_low"
-      @comics = Product.all.order(:price => :desc)
+      @comics = Comic.all.order(:price => :desc)
     elsif params["search-term"]
-      @comics = Product.where("name LIKE ?", "%#{params['search-term']}%")
+      @comics = Comic.where("name LIKE ?", "%#{params['search-term']}%")
     elsif params[:sort] == "discount_items"
-      @comics = Product.where("price < ?", 10)
+      @comics = Comic.where("price < ?", 10)
     else
-      @comics = Product.all
+      @comics = Comic.all
     end
-    # @comic_id = Product.find_by(params[])
+    # @comic_id = Comic.find_by(params[])
     render 'index.html.erb'
   end
 
@@ -20,11 +20,10 @@ class ComicsController < ApplicationController
   end
 
   def create
-    @comic1 = Product.new(
+    @comic1 = Comic.new(
       name: params["comic_name"],
       price: params["comic_price"],
-      description: params["comic_description"],
-      in_stock: params["in_stock"]
+      description: params["comic_description"]
     )
     @comic1.save
     flash[:success] = "Created comic"
@@ -34,34 +33,33 @@ class ComicsController < ApplicationController
   def show
     comic_id = params[:id]
     if params[:sort] == "random_item"
-      shuffled_products = Product.all.shuffle
+      shuffled_products = Comic.all.shuffle
       @comic = shuffled_products.first
     else
-      @comic = Product.find_by(id: comic_id)
+      @comic = Comic.find_by(id: comic_id)
     end
     render 'show.html.erb'
   end
 
   def edit
-    @comic = Product.find_by(id: params[:id])
+    @comic = Comic.find_by(id: params[:id])
     render 'edit.html.erb'
   end
 
   def update
-    @comic = Product.find_by(id: params[:id])
+    @comic = Comic.find_by(id: params[:id])
     @comic.update(
       name: params["comic_name"],
       price: params["comic_price"],
       image: params["comic_image"],
-      description: params["comic_description"],
-      in_stock: params["in_stock"]
+      description: params["comic_description"]
     )
     flash[:success] = "Updated comic"
     redirect_to "/comics/#{@comic.id}"
   end
 
   def destroy
-    @comic = Product.find(params[:id])
+    @comic = Comic.find(params[:id])
     @comic.destroy
     flash[:danger] = "Deleted comic."
     redirect_to "/comics"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,73 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+comic = Comic.create(
+  name: "X-Force #1",
+  price: 9.99,
+  description: "Liefeld - Nicieza",
+  image: "https://d1466nnw0ex81e.cloudfront.net/n_iv/600/858907.jpg"
+)
+
+comic = Comic.create(
+  name: "Spiderman RYV #4",
+  price: 3.99,
+  description: "Slott - Secret Wars",
+  image: "https://vignette2.wikia.nocookie.net/marveldatabase/images/b/b3/Amazing_Spider-Man_Renew_Your_Vows_Vol_1_4.jpg/revision/latest?cb=20150813224914"
+)
+
+comic = Comic.create(
+  name: "Criminal: Book 1",
+  price: 49.99,
+  description: "Brubaker - Phillips",
+  image: "https://images-na.ssl-images-amazon.com/images/I/91hsjgUvI6L.jpg"
+)
+
+comic = Comic.create(
+  name: "Preacher",
+  price: 29.99,
+  description: "Ennis - Dillon",
+  image: "https://images-na.ssl-images-amazon.com/images/I/71v7dphlO6L.jpg"
+)
+
+comic = Comic.create(
+  name: "Planet Hulk",
+  price: 19.99,
+  description: "Greg Pak",
+  image: "https://images-na.ssl-images-amazon.com/images/I/51sWFEf9CGL._SX323_BO1,204,203,200_.jpg"
+)
+
+comic = Comic.create(
+  name: "Batman - 50",
+  price: 4.50,
+  description: "Snyder - Capullo",
+  image: "https://static1.comicvine.com/uploads/original/0/6063/5110492-bm_cv50_ds.jpg"
+)
+
+comic = Comic.create(
+  name: "X-Men #1",
+  price: 8.75,
+  description: "Lee - Claremont",
+  image: "https://vignette2.wikia.nocookie.net/marveldatabase/images/3/33/X-Men_Vol_2_1_Magneto_Variant.jpg/revision/latest?cb=20091219024654"
+)
+
+comic = Comic.create(
+  name: "Watchmen",
+  price: 17.49,
+  description: "Alan Moore",
+  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Watchmen-cover.svg/2000px-Watchmen-cover.svg.png"
+)
+
+comic = Comic.create(
+  name: "Silver Surfer: Judgement Day",
+  price: 17.49,
+  description: "Lee - Buscema",
+  image: "http://2.bp.blogspot.com/_fZ37jdy-g4M/S8uvIgViTBI/AAAAAAAAA5A/je4ZB02X8R4/s1600/Ssjd-03.jpg"
+)
+
+comic = Comic.create(
+  name: "Superman: Red Son",
+  price: 7.99,
+  description: "Mark Millar",
+  image: "https://upload.wikimedia.org/wikipedia/en/d/dd/Supermanredson.jpg"
+)
